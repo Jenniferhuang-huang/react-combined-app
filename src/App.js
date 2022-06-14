b0f0439c9c1bdd189f10f9336cb77f62d74b61fa
Jiahui Huang <jiahuihuang@Jiahuis-MacBook-Pro.local>
2022-06-14 00:47:43 -0400

    navbar edit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import './App.css';
-import BarChart from './BarChart';
-import React from 'react';
+import "./App.css";
+import BarChart from "./BarChart";
+import React from "react";
 import Tree from "./Tree";
 import data from "./data";
-import { Link,BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Link, BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import { Canvas } from "@react-vertex/core";
 import { useViewportSize } from "@react-vertex/dom-hooks";
 import Scene from "./Scene";
-import Home from './Home';
+import Home from "./Home";
+import AppNavbar from "./Navbar";
 
 function Example() {
   const { width, height } = useViewportSize();
@@ -21,27 +22,20 @@ function Example() {
 }
 
 function TreeNode() {
-  return (
-    <Tree data={data} width={600} height={500} />
-  )
+  return <Tree data={data} width={600} height={500} />;
 }
 function App() {
   return (
     <Router>
-        <Switch>
-          <Route path='/' exact={true} component={Home} />
-          <Route path='/barchart' exact={true} component={BarChart} />
-          <Route path='/tree' exact={true} component={TreeNode} />
-          <Route path='/lightorb' exact={true} component={Example} />
-        </Switch>
-      </Router>
+      <AppNavbar />
+      <Switch>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/barchart" exact={true} component={BarChart} />
+        <Route path="/tree" exact={true} component={TreeNode} />
+        <Route path="/lightorb" exact={true} component={Example} />
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
-
-{/* <div>
-      <BarChart />
-      <Tree data={data} width={600} height={500} />
-      <Example />
-    </div> */}
